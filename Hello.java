1389a18f250f4f714db82feed48b3fd2a2a99f46
kommanab <55055284+kommanab@users.noreply.github.com>
2019-10-20 17:22:40 +0530

    updated remote
    remote

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
     public static void main(String args [])
     {
-        System.out.println("hello i am your lLove Bharath Reddy 2020");
+        System.out.println("hello i am your Love Bharath Reddy 2020");
     }
 }
